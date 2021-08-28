f8ae39c413985959853361aeac17775ad1673083
Martin L. Jensen <martinloesethjensen@gmail.com>
2021-08-28 13:53:35 +0200

    feat: added threshold and mappings

--- chill.js
+++ chill.js
@@ -15,24 +15,48 @@ async function main() {
 		api.query.staking.chillThreshold(),
 		api.query.staking.nominators.keys()
 	]);
-	  
+
+	const threshold = (chillThreshold / 100) * maxNominatorsCount;
+	
 	// extract the first key argument [AccountId] as string
 	const nominatorIds = nominatorKeys.map(({ args: [nominatorId] }) => nominatorId).slice(0,5);
+
+	// User should not be allowed to chillOther as this is not possible if it is below threshold.
+	// TODO: should be used after testing
+	/*
+	if (nominatorIds.length < threshold) {
+		console.log(`You can\'t chill others when nominators (${nominatorIds.length}) is below threshold (${threshold})`);
+		return;
+	}
+	*/
 	
+	const chillableAmount = nominatorIds.length - threshold;
+
 	console.log('The chillThreshold: ' + chillThreshold);
+	console.log('The threshold: ' + threshold);
+	console.log('The chillable amount: ' + chillableAmount);
 	console.log('The maxNominatorsCount: ' + maxNominatorsCount);
 	console.log('The current date is: ' + now);
-	console.log('The current minNominatorBond is: ' + minNominatorBond.toNumber());
+	console.log('The current minNominatorBond is: ' + minNominatorBond.toHuman());
 	console.log('all nominators:', nominatorIds.join('\n'));
-
+	
 	const unsub = await api.query.system.account.multi(nominatorIds, (balances) => {
-		let chillers = balances.filter(bal => bal.data.miscFrozen < minNominatorBond.toNumber());
+		let nominatorsBalancesBelow = new Map();
+		var index = 0;
+		for (balance of balances) {
+			if (balance.data.miscFrozen < minNominatorBond.toNumber()) {
+				nominatorsBalancesBelow.set(nominatorIds[index], balance);
+			}
+			index++;
+		}
 
-		console.log(`The balances are ${balances[0]}`);
-		console.log(`The chillers are ${chillers.length}`);
+		for (const [addr, balance] of nominatorsBalancesBelow) {
+			console.log('nominator:', addr.toHuman());
+			console.log('miscFrozen:', balance.data.miscFrozen.toHuman());
+		}
 	});
 
-	console.log('Nominators:', nominatorIds.length);
+	console.log('Total nominators:', nominatorIds.length);
 
 	await new Promise(resolve => setTimeout(resolve, 3000));
 }
