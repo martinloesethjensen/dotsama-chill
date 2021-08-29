3d691dd91e10ae173125faf7769e278d60fb4484
Alex Plutta <alex.plutta@googlemail.com>
2021-08-29 17:01:43 +0200

    add tailwind

---
+++ src/ChillApp.jsx
@@ -0,0 +1,5 @@
+export const ChillApp = ({}) => {
+    return <div className="flex justify-center">
+        <p>Hello chill app </p>
+    </div>
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import "./index.css";
 
-ReactDOM.render(<App />,
-  document.getElementById('root')
+import App from './App';
+import {ChillApp} from "./ChillApp";
+
+ReactDOM.render(<ChillApp/>,
+    document.getElementById('root')
 );
