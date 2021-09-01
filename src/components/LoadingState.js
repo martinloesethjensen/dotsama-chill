ed0cb75e220e4f1433a94195f861d83fe34c1d78
Martin L. Jensen <martinloesethjensen@gmail.com>
2021-09-02 00:53:30 +0200

    feat: dino game on loading

--- src/components/LoadingState.js
+++ src/components/LoadingState.js
@@ -1,4 +1,6 @@
 import React from "react"
 
 //Todo add fancy loading state!!!
-export const LoadingState = () => <p>Validators are being loaded </p>
+export const LoadingState = () => {
+    return <iframe src="https://chromedino.com/" frameborder="0" scrolling="no" width="100%" height="100%" loading="lazy"></iframe>
+}
