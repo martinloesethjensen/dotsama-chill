52c669e2ddf09125ad7cf4c963e95fb97f37afe4
Alex Plutta <alex.plutta@googlemail.com>
2021-09-04 00:38:49 +0200

    add new statistics

--- src/components/statistics/StatisticsBox.jsx
+++ src/components/statistics/StatisticsBox.jsx
@@ -1,52 +1,67 @@
 import React from "react";
-import { StatisticsGroup } from "./StatisticsGroup";
+import {StatisticsGroup} from "./StatisticsGroup";
 
 export const StatisticsBox = ({
-  chillableAmount,
-  nominatorIds,
-  threshold,
-  minNominatorBond,
-  currentEra,
-  maxNominatorsCount,
-  isLoading,
-}) => {
-  const minNominatorBondOrDefault = () =>
-    isLoading ? "Loading" : minNominatorBond.toHuman();
-  const nominatorIdsOrDefault = () =>
-    isLoading ? "Loading" : nominatorIds.length;
-  const chillableAmountOrDefault = () =>
-    isLoading ? "Loading" : chillableAmount;
-  const thresholdOrDefault = () => (isLoading ? "Loading" : threshold);
-  const currentEraOrDefault = () =>
-    isLoading ? "Loading" : currentEra.toHuman();
-  const maxNominatorsCountOrDefault = () =>
-    isLoading ? "Loading" : maxNominatorsCount.toHuman();
-  return (
-    <div className="flex justify-between mb-8">
-      <StatisticsGroup
-        first={{
-          name: "Threshold",
-          count: thresholdOrDefault(),
-          textColor: "red-500",
-        }}
-      />
-      <div></div>
-      <StatisticsGroup
-        first={{
-          name: "Max Chillable Amount",
-          count: chillableAmountOrDefault(),
-          textColor: "yellow-500",
-        }}
-      />
-      <div></div>
+                                  chillableAmount,
+                                  nominatorIds,
+                                  threshold,
+                                  minNominatorBond,
+                                  currentEra,
+                                  maxNominatorsCount,
+                                  isLoading,
+                              }) => {
+    const minNominatorBondOrDefault = () =>
+        isLoading ? "Loading" : minNominatorBond.toHuman();
+    const nominatorIdsOrDefault = () =>
+        isLoading ? "Loading" : nominatorIds.length;
+    const chillableAmountOrDefault = () =>
+        isLoading ? "Loading" : chillableAmount;
+    const thresholdOrDefault = () => (isLoading ? "Loading" : threshold);
+    const currentEraOrDefault = () =>
+        isLoading ? "Loading" : currentEra.toHuman();
+    const maxNominatorsCountOrDefault = () =>
+        isLoading ? "Loading" : maxNominatorsCount.toHuman();
+    return (
+        <div className="flex justify-between mb-8">
+            <StatisticsGroup
+                first={{
+                    name: "Threshold",
+                    count: thresholdOrDefault(),
+                    textColor: "red-500",
+                }}
+                second={{
+                    name: "Nominator Ids",
+                    count: nominatorIdsOrDefault(),
+                    textColor: "red-500",
+                }}
+            />
+            <div></div>
+            <StatisticsGroup
+                first={{
+                    name: "Max Chillable Amount",
+                    count: chillableAmountOrDefault(),
+                    textColor: "yellow-500",
+                }}
+                second={{
+                    name: "Current Era",
+                    count: currentEraOrDefault(),
+                    textColor: "yellow-500",
+                }}
+            />
+            <div></div>
 
-      <StatisticsGroup
-        first={{
-          name: "Minimum Nominator Bond",
-          count: minNominatorBondOrDefault(),
-          textColor: "blue-500",
-        }}
-      />
-    </div>
-  );
+            <StatisticsGroup
+                first={{
+                    name: "Minimum Nominator Bond",
+                    count: minNominatorBondOrDefault(),
+                    textColor: "blue-500",
+                }}
+                second={{
+                    name: "Maximum Nominator Count",
+                    count: maxNominatorsCountOrDefault(),
+                    textColor: "blue-500",
+                }}
+            />
+        </div>
+    );
 };
--- src/components/statistics/StatisticsGroup.jsx
+++ src/components/statistics/StatisticsGroup.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {Statistic} from "./Statistic";
 
 export const StatisticsGroup = ({first, second}) => {
-    return <div className="gap-8  bg-white border-white rounded-2xl mb-4 p-12 px-16 grid grid-cols-2">
+    return <div className="gap-8  bg-white border-white rounded-2xl mb-4 p-12 px-16 grid grid-rows-2">
         <Statistic {...first}/>
         <Statistic {...second}/>
     </div>
