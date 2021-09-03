b2bf798d39b70b8cdf7dbba6cbafa618bf450759
Alex Plutta <alex.plutta@googlemail.com>
2021-09-03 23:59:59 +0200

    change colors depending the network

--- src/components/table/SortButton.jsx
+++ src/components/table/SortButton.jsx
@@ -1,10 +1,12 @@
 import React, {useContext, useState} from "react";
 import Switch from "react-switch";
 import {NominatorsContext} from "../../context/NominatorsContext";
+import {SUPPORTED_NETWORKS} from "../../utils/setProvider";
+import {COLORS} from "../../constants";
+import {NetworkContext} from "../../context/NetworkContext";
 
 export const SortButton = ({}) => {
-
-
+    const {selectedNetwork} = useContext(NetworkContext);
     const {setNominators} = useContext(NominatorsContext);
 
     const SORT_ORDER = Object.freeze({
@@ -14,6 +16,9 @@ export const SortButton = ({}) => {
     const [sortOrder, setSortOrder] = useState(SORT_ORDER.ASC)
 
     const getSortOrderName = () => `${sortOrder === SORT_ORDER.ASC ? "ASC" : "DESC"}`
+
+    const buttonColor = selectedNetwork === SUPPORTED_NETWORKS.POLKADOT ? COLORS.POLKADOT : COLORS.KUSAMA;
+
 
     const sortNominators = order => {
         setSortOrder(order);
@@ -30,8 +35,8 @@ export const SortButton = ({}) => {
     return <div className="flex items-center ">
         <p className="pr-2 ">{getSortOrderName()}</p>
         <Switch onChange={sortNominators}
-                onColor="#E6007A"
-                offColor="#E6007A"
+                onColor={buttonColor}
+                offColor={buttonColor}
                 checked={sortOrder}
                 checkedIcon={false}
                 uncheckedIcon={false}
