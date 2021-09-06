8b134a1d62579e8e4d438dd0837ba11922c1771e
Martin L. Jensen <martinloesethjensen@gmail.com>
2021-09-06 15:04:07 +0200

    feat: added Polkadot & Kusama wss endpoints from elara.patract.io

--- src/utils/setProvider.js
+++ src/utils/setProvider.js
@@ -1,9 +1,9 @@
 export const getEndpointForNetwork = provider => {
     switch (provider) {
         case SUPPORTED_NETWORKS.POLKADOT:
-            return "wss://rpc.polkadot.io";
+            return "wss://service.elara.patract.io/Polkadot/88475bd54c97c29e56946c5340382120";
         case SUPPORTED_NETWORKS.KUSAMA :
-            return "wss://kusama-rpc.polkadot.io";
+            return "wss://service.elara.patract.io/Kusama/ed8790d0226590b545a7af8b5518066c";
         default:
             throw Error(`Unknown Provider ${provider}`);
     }
